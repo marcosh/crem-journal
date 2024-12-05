e0f12cd94d07c673579349f39c224ae24dbcadae
Marco Perone <pasafama@gmail.com>
2024-12-05 09:28:55 +0100

    sketch data structure for csv file based repository

---
+++ src/Infrastructure/Read/CsvFileJournalRepository.hs
@@ -0,0 +1,31 @@
+module Infrastructure.Read.CsvFileJournalRepository where
+
+import "base" GHC.Generics (Generic)
+import "bytestring" Data.ByteString.Char8 (pack, unpack)
+import "cassava" Data.Csv (Field, FromField (..), FromRecord, Parser, ToField (..), ToRecord)
+import "text" Data.Text (Text)
+import "time" Data.Time (UTCTime)
+import "time" Data.Time.Format.ISO8601 (iso8601ParseM, iso8601Show)
+
+newtype CsvTime = CsvTime UTCTime
+  deriving newtype (Show)
+
+instance FromField CsvTime where
+  parseField :: Field -> Parser CsvTime
+  parseField field =
+    case iso8601ParseM $ unpack field of
+      Nothing -> fail "date not in ISO8601 format"
+      Just time -> pure $ CsvTime time
+
+instance ToField CsvTime where
+  toField :: CsvTime -> Field
+  toField (CsvTime time) = pack $ iso8601Show time
+
+data CsvRow = JournalCsvRow
+  { content :: Text
+  , createdAt :: CsvTime
+  }
+  deriving stock (Generic, Show)
+
+instance FromRecord CsvRow
+instance ToRecord CsvRow
