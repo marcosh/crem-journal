7b750ecbcaf89f4d26144c2021f765aadc50bf7f
Marco Perone <pasafama@gmail.com>
2024-12-30 17:41:51 +0100

    keep list of events in aggregate state

--- src/Domain/Write/JournalAggregate.hs
+++ src/Domain/Write/JournalAggregate.hs
@@ -1,8 +1,9 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE MonoLocalBinds #-}
 
 module Domain.Write.JournalAggregate where
 
-import "crem" Crem.BaseMachine (BaseMachineT, statelessBaseT)
+import "crem" Crem.BaseMachine (ActionResult (..), BaseMachineT (..), InitialState (..))
 import "crem" Crem.Topology (TrivialTopology)
 import "monad-time" Control.Monad.Time (MonadTime)
 
@@ -11,6 +12,19 @@ import Domain.Write.JournalEntry (JournalEntry (..))
 import Domain.Write.JournalEntryCreatedAt (journalCreatedNow)
 import Domain.Write.JournalEvent (JournalEvent (..))
 
+data JournalState (journalVertex :: ()) where
+  JournalMapState :: [JournalEvent] -> JournalState '()
+
 aggregate :: (MonadTime m) => BaseMachineT m (TrivialTopology @()) JournalCommand JournalEvent
-aggregate = statelessBaseT $ \(RecordJournalEntry content) ->
-  JournalEntryRecorded . JournalEntry content <$> journalCreatedNow
+aggregate =
+  BaseMachineT
+    { initialState = InitialState $ JournalMapState []
+    , action = \(JournalMapState events) (RecordJournalEntry content) ->
+        ActionResult $ do
+          journalEntryCreatedAt <- journalCreatedNow
+          let event = JournalEntryRecorded (JournalEntry content journalEntryCreatedAt)
+          pure
+            ( event
+            , JournalMapState $ event : events
+            )
+    }
